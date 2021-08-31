f451a094fce96cb3f05ea3f0ef77baccb764809a
henockjoy65 <89825644+henockjoy65@users.noreply.github.com>
2021-08-31 10:02:34 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @FT_Channels" ,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Our Group', url="https://t.me/FT_Chats"
                                 )
                         ]
                     ]
